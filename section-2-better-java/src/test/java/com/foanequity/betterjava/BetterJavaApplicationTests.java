6ac74ae9c6531d7b462f7836a93ee92f72a0768d
quickheaven <quckheaven@gmail.com>
2020-03-17 17:59:54 -0400

    Section 2: Better Java

---
+++ section-2-better-java/src/test/java/com/foanequity/betterjava/BetterJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.foanequity.betterjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BetterJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
